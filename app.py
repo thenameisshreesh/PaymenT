c799da874d5081b4dc6a6afc1cf3b63c0ec5f159
thenameisshreesh <shreeshpitambare084@gmail.com>
2025-07-29 01:08:38 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def pay():
             "customer_phone": mobile
         },
         "order_meta": {
-        "return_url": f"https://payment-production-a756.up.railway.app/payment_status?order_id={order_id}"
+        "return_url": f"https://payment-production-a8af.up.railway.app/payment_status?order_id={order_id}"
             
         }
     }
